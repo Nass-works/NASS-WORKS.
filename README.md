d3096e1f9005ced7bf8507a0641ebac51c84b82c
NASS-WORKS <117126055+Nass-works@users.noreply.github.com>
2022-11-26 15:15:33 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,68 @@
-# NASS-WORKS.
+<p align="center">
+   <a href="https://github.com">
+    <img src="https://media.giphy.com/media/RbDKaczqWovIugyJmW/giphy.gif" width="610">
+     
+</p>
+<h1 align="center"> NASS-WORKS
+</h1>
+<p align="center"> 
+  Hello, I am "NASS-WORKS" a WhatsApp bot create and recode by Nass to do everything that is possible on WhatsApp based on WhatsApp Multi Device(MD) Support.
+
+<p align="center">
+  <a href="https://github.com/Kai0071/A17/fork">
+    <img src="https://img.shields.io/github/forks/Kai0071/A17?label=Fork&style=social">
+    
+   <p align="center"> 
+  <a href="https://github.com/Kai0071/A17/stargazers">
+    <img src="https://img.shields.io/github/stars/Kai0071/A17?style=social">
+
+ 
+  <p align="CENTER">
+  <a href="https://github.com/Kai0071"><img title="Support" src="https://img.shields.io/badge/Maintain-Yes-cyan.svg?style=for-the-badge&logo=xcode" /></a>
+</p>
+    
+
+     
+  
+<p align="CENTER">
+  <a href="https://github.com/Kai0071"><img title="Support" src="https://img.shields.io/badge/next%20Update-Undefined!-green.svg?style=for-the-badge&logo=xcode" /></a>
+</p>
+      
+ 
+  <p align="center">
+<a href="https://replit.com/@Broken0007/A17-QR-Scanner?v=1"><img title="NASS-WORKS" src="https://repl.it/badge/github/quiec/whatsasena" width="400"></a>
+</p>
+
+
+
+<h2 align="center"> ✧ Join My Whatsapp Group for Support ✧
+</h2>
+
+<h2 align="center">  <a href="https://chat.whatsapp.com/LIriWFvmyyp9RakyOh1zyV"><img src="https://img.shields.io/badge/Join Group-25D366?style=for-the-badge&logo=whatsapp&logoColor=white" />
+</a>
+
+</h2>
+
+
+
+
+
+</br> 
+
+<h2 align="center">  Reminder
+</h2>
+   
+## 
+- This bot is not made by `WhatsApp Inc.` So misusing the bot might `ban` your `WhatsApp account!`(Though your WhatsApp account can be unbanned only once.)
+- I am not responsible for banning your account.
+- Use at your own risk by keeping this warning in mind.
+ 
+
+
+<h1 align="center">
+</h1>
+
+
+</p>
+<h1 align="center" style="color: Red";> ✧ Thanks To Nass and Fantox ✧
+</h1>
---
+++ config.js
@@ -0,0 +1,115 @@
+const fs = require("fs")
+const chalk = require("chalk")
+
+global.available = true
+global.autoReadGc = true
+global.autoReadAll = false
+global.antitags = true
+
+
+global.Owner = ['2348123433273']
+global.OwnerNumber = ['2348123433273']
+global.ownertag = ['2348123433273']
+global.BotName = "NASS-WORKS"
+global.packname = "NASS-WORKS"
+global.author = "By: Nass"
+global.OwnerName = "NASS-WORKS"
+global.BotSourceCode = "nass/works@gmail.com"
+global.SupportGroupLink = "https://chat.whatsapp.com/LIriWFvmyyp9RakyOh1zyVl"
+global.sessionName = "session"
+
+// Prefix //
+// If you want to change the prefix, change it and also change all switch's button id from Core.js  otherwise buttons will not work //
+global.prefa = ['-'] 
+
+
+global.location = "NIGERIA, ABUJA"
+global.reactmoji = "❤️"
+global.themeemoji = "💖"
+global.vidmenu = { url: 'https://media.tenor.com/Jdu0Ov8X2sIAAAAC/A17-Bot.mp4' }
+global.websitex = "https://github.com"
+global.lolhuman = "Nass"
+
+
+global.BotLogo = fs.readFileSync("./Assets/pic1.jpg")
+global.Thumb = fs.readFileSync("./Assets/pic9.jpg")
+global.Thumb1 = fs.readFileSync("./Assets/pic5.jpg")
+global.ErrorPic = fs.readFileSync("./Assets/pic7.jpg")
+
+
+global.ntilinkytvid = []
+global.ntilinkytch = []
+global.ntilinkig = []
+global.ntilinkfb = []
+global.ntilinktg = []
+global.ntilinktt = []
+global.ntilinktwt = []
+global.ntilinkall = []
+global.nticall = []
+global.ntwame = []
+global.nttoxic = []
+global.ntnsfw = []
+global.ntvirtex = []
+global.rkyt = []
+global.wlcm = []
+global.gcrevoke = []
+global.autorep = []
+global.ntilink = []
+
+
+global.mess = {
+    jobdone: 'Job done...',
+    useradmin: 'Hmm...Sorry, only *Group Admins* can use this command *Dear*!',
+    botadmin: 'Sorry, I cant execute this command without being an *Admin* of this group.',
+    botowner: 'Only *Nass* can use this command, bwahahaha!',
+    grouponly: 'This command is only made for *Groups*, Hope ya get that!',
+    privateonly: 'This command is only made for *Private Chat*, Okey ?!',
+    botonly: 'Only the *Bot itself* can use this command!',
+    waiting: ' Here you go...',
+    nolink: 'Please provide me *link*, Uff!',
+    error: 'An error occurd!',
+    banned: 'You are *Banned* fron using commands!, Haha',
+    bangc: 'Nass has *Banned* this group from using Commands!',
+    nonsfw: 'Dont be a pervert  Uff! This is not a NSFW enabled group!, Uff!,'
+    
+}
+
+
+global.limitawal = {
+    premium: "Infinity",
+    free: 2,
+    monayawal: 1000
+}
+   global.rpg = {
+   darahawal: 100,
+   besiawal: 95,
+   goldawal: 30,
+   emeraldawal: 8,
+   umpanawal: 10,
+   potionawal: 5
+}
+global.limitawal = {
+    rakyat: "Infinity",
+    free: 100
+}
+
+
+global.APIs = {
+	zenz: 'https://zenzapis.xyz',
+}
+global.APIKeys = {
+	'https://zenzapis.xyz': '5d1197db351b',
+}
+
+
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
